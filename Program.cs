5c7b2e059db486b843c375b6aa98de95d076fac9
Julie Lerman <julielerman@users.noreply.github.com>
2017-10-10 15:12:07 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace AngularMix2017
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
     }
