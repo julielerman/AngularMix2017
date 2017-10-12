812f5b6f937c14917f2a6dfd81b32555c06bf9c1
julie <julielerman@gmail.com>
2017-10-11 22:51:16 -0400

    add json object support

--- FireflyService.cs
+++ FireflyService.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using Firefly.Data;
 using Firefly.Domain;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
 
 public class FireflyService {
   private CharacterContext _context;
@@ -13,76 +17,92 @@ public class FireflyService {
   public FireflyService (CharacterContext context) {
     _context = context;
   }
-  
-  public Character CreateNewCharacter (string name) {
+
+  public string CreateNewCharacter (string name) {
     var character = new Character (name);
     _context.Characters.Add (character);
     _context.SaveChanges ();
-    return character;
+    return character.Id.ToString ();
   }
-  
-  public Character CreateNewCharacter (string name, int entranceMinute, string entranceSceneName, string entranceDescription) { 
-    var character = new Character (name); 
+
+  public Character CreateNewCharacter (string name, int entranceMinute, string entranceSceneName, string entranceDescription) {
+    var character = new Character (name);
     character.CreateEntrance (entranceMinute, entranceSceneName, entranceDescription);
     _context.Characters.Add (character);
     _context.SaveChanges ();
     return character;
   }
-
-  public  int AddQuoteToCharacter(int characterId, string quoteText) {
-    var character = GetCharacter (characterId);
-    if(character is null){
-     return 0;
+  public string InsertCharacterGraph (CharacterViewModel characterJson) {
+    if (characterJson.CharacterName == null) { return "Invalid json"; }
+    var character = new Character (characterJson.CharacterName);
+    var entrance = characterJson.Entrance;
+    if (entrance != null) {
+      character.CreateEntrance (entrance.MovieMinute, entrance.SceneName, entrance.Description);
     }
-    character.AddQuote (quoteText);
-    _context.Entry(character.Quotes.First()).State=EntityState.Added;
-    return  _context.SaveChanges();
-  }
-
-  public int AddEntranceToCharacter (int characterId, int entranceMinute, string entranceSceneName, string entranceDescription) {
-    var character = _context.Characters.Include("Entrance").FirstOrDefault(c=>c.Id==characterId); 
-    if(character is null){ 
-      return 0; 
-     } 
-     if(character.HasEntrance){
-       return 0;
-     }
-    character.CreateEntrance (entranceMinute, entranceSceneName, entranceDescription);
-    return  _context.SaveChanges();
-  }
-
-  public int ReplaceEntranceForCharacter (int characterId, int entranceMinute, string entranceSceneName, string entranceDescription) {
-    var character = _context.Characters.Include("Entrance").FirstOrDefault(c=>c.Id==characterId);
-    if(character is null){
-     return 0;
+    foreach (var quote in characterJson.Quotes) {
+      if (quote.Text!=null){
+        character.AddQuote (quote.Text);
+      }
     }
-    if(character.HasEntrance){
-     character.ReplaceEntrance (entranceMinute, entranceSceneName, entranceDescription);
-    }
-    return  _context.SaveChanges();
+    _context.Characters.Add (character);
+    _context.SaveChanges ();
+    return character.Id.ToString ();
   }
 
-  public Character GetFullCharacterDetails (int characterId) {
-    var character = _context.Characters.Include(c=>c.Quotes).Include("Entrance")
-      .FirstOrDefault (c => c.Id == characterId);
-    return character;
+public int AddQuoteToCharacter (int characterId, string quoteText) {
+  var character = GetCharacter (characterId);
+  if (character is null) {
+    return 0;
   }
+  character.AddQuote (quoteText);
+  _context.Entry (character.Quotes.First ()).State = EntityState.Added;
+  return _context.SaveChanges ();
+}
 
-  public List<String[]> GetListOfCharacterNamesAndIds () {
-    return _context.Characters.Select (c => new String[] { c.Name, c.Id.ToString() }).ToList ();
+public int AddEntranceToCharacter (int characterId, int entranceMinute, string entranceSceneName, string entranceDescription) {
+  var character = _context.Characters.Include ("Entrance").FirstOrDefault (c => c.Id == characterId);
+  if (character is null) {
+    return 0;
   }
+  if (character.HasEntrance) {
+    return 0;
+  }
+  character.CreateEntrance (entranceMinute, entranceSceneName, entranceDescription);
+  return _context.SaveChanges ();
+}
 
-  public async Task<int>  AddQuoteToCharacterAsync (int characterId, string quoteText) {
-    var character = GetCharacter (characterId);
-    if(character is null){
-     return 0;
-    }
-    character.AddQuote (quoteText);
-    return await _context.SaveChangesAsync ();
+public int ReplaceEntranceForCharacter (int characterId, int entranceMinute, string entranceSceneName, string entranceDescription) {
+  var character = _context.Characters.Include ("Entrance").FirstOrDefault (c => c.Id == characterId);
+  if (character is null) {
+    return 0;
   }
+  if (character.HasEntrance) {
+    character.ReplaceEntrance (entranceMinute, entranceSceneName, entranceDescription);
+  }
+  return _context.SaveChanges ();
+}
 
-  private Character GetCharacter (int characterId) {
-    return _context.Characters.Find(characterId);
-    
+public Character GetFullCharacterDetails (int characterId) {
+  var character = _context.Characters.Include (c => c.Quotes).Include ("Entrance")
+    .FirstOrDefault (c => c.Id == characterId);
+  return character;
+}
+
+public List<String[]> GetListOfCharacterNamesAndIds () {
+  return _context.Characters.Select (c => new String[] { c.Name, c.Id.ToString () }).ToList ();
+}
+
+public async Task<int> AddQuoteToCharacterAsync (int characterId, string quoteText) {
+  var character = GetCharacter (characterId);
+  if (character is null) {
+    return 0;
   }
+  character.AddQuote (quoteText);
+  return await _context.SaveChangesAsync ();
+}
+
+private Character GetCharacter (int characterId) {
+  return _context.Characters.Find (characterId);
+
+}
 }
